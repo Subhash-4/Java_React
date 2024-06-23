c9751e8d715f290a02feb878a370c897d073dff3
Subhash-4 <subhashsanadi2000@Gmail.com>
2024-06-23 16:01:35 +0530

    Insurance

---
+++ Insurance.java
@@ -0,0 +1,59 @@
+package com.java.firstDay;
+
+import java.util.Scanner;
+
+public class Insurance {
+	
+	public static void main(String[] args) {
+		
+		Insurance det=new Insurance();
+		det.details();
+		System.out.println("exit");	
+	}
+	
+	public void details(){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter id");
+		int id=sc.nextInt();
+		
+		System.out.println("enter a name");
+		String name=sc.next();
+		
+		
+		System.out.println("enter age");
+		int age=sc.nextInt();
+		
+		System.out.println("Enter Gender (M/F):");
+        	String gender = sc.next();
+		
+		
+		System.out.println("salary");
+		int salary=sc.nextInt();
+		
+		System.out.println("enter designation");
+		String designation=sc.next();
+		
+		System.out.println("____________");
+	        System.out.println("id: " + id);
+	        System.out.println("Name: " + name);
+	        System.out.println("Gender: " + gender);
+	        System.out.println("Age: " + age);
+	        System.out.println("salary: " + salary);
+	        System.out.println("designation: " + designation);
+	        
+        if(salary>5000 && salary<20000) {
+			System.out.println("insurence belongs to C scheme");
+		}
+		else if(salary>=20000 && salary<40000){
+			System.out.println("insurence belongs to b scheme");
+		}
+		else if(salary>=40000) {
+			System.out.println("insurence belongs to A scheme");
+		}
+		else {
+			System.out.println("no scheme");
+		}
+		sc.close();
+		
+	}
+}
